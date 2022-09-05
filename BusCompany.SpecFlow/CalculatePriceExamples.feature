115cdc809344d5cdcace80637a14d6685fbfea11
Henrik Kühl <henrikkyhl@hotmail.com>
2022-09-05 23:16:18 +0200

    Initial commit

---
+++ BusCompany.SpecFlow/CalculatePriceExamples.feature
@@ -0,0 +1,24 @@
+﻿Feature: CalculatePriceExamples
+    In order to be able to create an invoice
+    As an office clerk at the bus company
+    I want to be able to calculate the total price for having rented a bus
+
+Scenario Outline: Calculate price
+    Given I have entered a distance of <distance> km.
+    And I have also selected a <bustype> bus
+    When I press Calculate
+    Then the result should be <price>
+
+    Examples:
+    | distance | bustype    | price  |
+    | 0        | 'Standard' | 2500   |
+    | 99       | 'Luxory'   | 4735   |
+    | 100      | 'Mini'     | 2598.8 |
+    | 500      | 'Standard' | 6698   |
+    | 501      | 'Standard' | 6704   |
+
+
+
+    Scenario: Calculate price with negative distance
+        When I calculate with negative distance
+        Then An error should occur
---
+++ BusCompany.SpecFlow/CalculatePriceSteps.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace BusCompany.SpecFlow
+{
+    [Binding]
+    //[Scope(Feature = "CalculatePriceExamples")]
+    public class CalculatePriceSteps
+    {
+        private int distance;
+        private BusType bustype;
+        private decimal price;
+        private Action action;
+        private PriceCalculator calc = new PriceCalculator();
+
+        [Given(@"I have entered a distance of (.*) km\.")]
+        public void GivenIHaveEnteredADistanceOfKm_(int distance)
+        {
+            this.distance = distance;
+        }
+
+        [Given(@"I have also selected a '(.*)' bus")]
+        public void GivenIHaveAlsoSelectedABus(BusType bustype)
+        {
+            this.bustype = bustype;
+        }
+
+        [When(@"I press Calculate")]
+        public void WhenIPressCalculate()
+        {
+            price = calc.CalculatePrice(distance, bustype);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(decimal expectedPrice)
+        {
+            Assert.Equal(expectedPrice, price);
+        }
+        
+        [When(@"I calculate with negative distance")]
+        public void WhenICalculateWithNegativeDistance()
+        { 
+            action = () => calc.CalculatePrice(-1, BusType.Standard);
+        }
+
+        [Then(@"An error should occur")]
+        public void ThenAnErrorShouldOccur()
+        {
+            Assert.Throws<ArgumentException>(action);
+        }
+    }
+}
